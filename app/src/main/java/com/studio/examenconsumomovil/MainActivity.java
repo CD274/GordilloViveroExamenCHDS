62343459117174a408d6cb997b03f0f1f20bcf95
Carlos <gordillod274@gmail.com>
2024-12-16 10:52:31 -0500

    Consumo de web service con express con constante GET

--- app/src/main/java/com/studio/examenconsumomovil/MainActivity.java
+++ app/src/main/java/com/studio/examenconsumomovil/MainActivity.java
@@ -1,6 +1,7 @@
 package com.studio.examenconsumomovil;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,14 +14,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText textArea;
     private Button btnEnviar;
+    JSONArray array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         textArea = findViewById(R.id.text_area);
         btnEnviar = findViewById(R.id.btn_Enviar);
+
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,23 +47,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void recibirMensaje() {
-        String url = "http://10.10.35.19:3000/Gordillo";
+        String url = "http://10.10.35.19:3000/nombre";
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        textArea.setText(response);
+                    public void onResponse(JSONArray response) {
+                        StringBuilder mensaje = new StringBuilder();
+
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject obj = response.getJSONObject(i);
+                                String id = obj.getString("id");
+                                String nombre = obj.getString("nombre");
+
+                                mensaje.append("ID: ").append(id)
+                                        .append(", Nombre: ").append(nombre)
+                                        .append("\n");
+                            }
+
+                            textArea.setText(mensaje.toString());
+
+                        } catch (JSONException e) {
+                            Toast.makeText(MainActivity.this, "Error al procesar datos", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(MainActivity.this, "Error al obtener datos", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
                     }
                 });
-
-        queue.add(stringRequest);
+        queue.add(jsonArrayRequest);
     }
+
 }
